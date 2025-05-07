a9a7148c159eb9538cddd585e17c57fe627c7a87
allaoui2025 <faridallaoui65@gmail.com>
2025-05-07 10:10:59 +0100

    Update UserController.java

--- src/main/java/com/demo/bankapp/controller/UserController.java
+++ src/main/java/com/demo/bankapp/controller/UserController.java
@@ -1,30 +1,46 @@
 package com.demo.bankapp.controller;
 
-import com.demo.bankapp.constant.Constants;
-import com.demo.bankapp.dto.request.CreateUserRequest;
-import com.demo.bankapp.dto.response.CreateUserResponse;
-import com.demo.bankapp.exception.BadRequestException;
-import com.demo.bankapp.model.User;
-import com.demo.bankapp.service.UserService;
-import com.demo.bankapp.service.WealthService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
 import java.util.regex.Pattern;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.bankapp.configuration.Constants;
+import com.demo.bankapp.exception.BadCredentialsException;
+import com.demo.bankapp.exception.BadRequestException;
+import com.demo.bankapp.model.User;
+import com.demo.bankapp.request.CreateUserRequest;
+import com.demo.bankapp.response.CreateUserResponse;
+import com.demo.bankapp.response.FindAllUsersResponse;
+import com.demo.bankapp.service.abstractions.IUserService;
+import com.demo.bankapp.service.abstractions.IWealthService;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping(value = "/user", produces = { MediaType.APPLICATION_JSON_VALUE })
 public class UserController {
 
-    private final UserService userService;
-    private final WealthService wealthService;
+    private final IUserService userService;
+    private final IWealthService wealthService;
 
     @Autowired
-    public UserController(UserService userService, WealthService wealthService) {
+    public UserController(IUserService userService, IWealthService wealthService) {
         this.userService = userService;
         this.wealthService = wealthService;
+    }
+
+    @GetMapping("/find/all")
+    public FindAllUsersResponse findAll() {
+        List<User> userList = userService.findAll();
+
+        FindAllUsersResponse response = new FindAllUsersResponse();
+        response.setUserList(userList);
+        return response;
     }
 
     @PostMapping("/create")
@@ -57,5 +73,11 @@ public class UserController {
         response.setUsername(user.getUsername());
         response.setTcno(user.getTcno());
         return response;
+    }
+
+    // Debug only - remove in production!
+    @GetMapping("/debug/creds")
+    public String getDebugCreds() {
+        return "Username: admin, Password: admin123";
     }
 }
